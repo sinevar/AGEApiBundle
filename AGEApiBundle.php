6581f9feb3ad6168a2bcf35f5e2155d9e573a2a8
sinevar <adam.gegotek@gmail.com>
2012-06-14 16:05:56 +0200

    Bundle skeleton

---
+++ AGEApiBundle.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is a part of AGEApiBundle package.
+ *
+ * (c) Adam Gegotek <adam.gegotek@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AGE\ApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Adam Gegotek <a.gegotek@gmail.com> 
+ */
+class AGEApiBundle extends Bundle
+{
+}
