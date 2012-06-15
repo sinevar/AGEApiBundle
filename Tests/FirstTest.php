b7e06cc76919d6fba4b830732371db7f03b9884f
sinevar <adam.gegotek@gmail.com>
2012-06-15 13:40:59 +0200

    adding first test

---
+++ Tests/FirstTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AGE\ApiBundle\Tests;
+
+class FirstTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAdd()
+    {
+        $result = 'ItemSearch'; 
+        $this->assertEquals('ItemSearch', $result);
+    }  
+}
+
