8f1516b5b9e69f6266ed797f82116f987a241ed3
sinevar <adam.gegotek@gmail.com>
2012-06-15 14:09:27 +0200

    adding second test

---
+++ Tests/SecondTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AGE\ApiBundle\Tests;
+
+class SecondTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAdd()
+    {
+        $result = 10; 
+        $this->assertEquals(10, $result);
+    }  
+}
